863d04486ded297b9e3394ca676971ab3466f1c5
Vinson Li <97509395+CaptialK@users.noreply.github.com>
2022-11-05 18:02:07 -0700

    New update
    complete refresh

---
+++ animations/js/animations.js
@@ -0,0 +1,14 @@
+
+
+function getPics() {} //just for this demo
+const imgs = document.querySelectorAll('.gallery img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
+
+console.log('getPics')
